78706e091055a63f5bfb30eb409420905074e323
Rich Kuzsma <rkuzsma@constantcontact.com>
2018-04-05 05:11:26 -0400

    Copy CSS static assets to production build dir

--- hipchat/webpack.prod.js
+++ hipchat/webpack.prod.js
@@ -1,10 +1,16 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = merge(common, {
   output: {
     // Tweak this to match your GitHub project name
     publicPath: "/cornchat/",
   },
+  plugins: [
+     new CopyWebpackPlugin([
+         { from: 'src/assets' }
+     ])
+ ],
   mode: 'production'
 });
