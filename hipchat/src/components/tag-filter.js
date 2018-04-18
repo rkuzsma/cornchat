a21110e1086d303ff776385fd3daa56ff9b41b67
Rich Kuzsma <rkuzsma@constantcontact.com>
2018-04-18 07:35:17 -0400

    Hook up Filter by tag and clear filter

--- hipchat/src/components/tag-filter.js
+++ hipchat/src/components/tag-filter.js
@@ -1,18 +1,54 @@
 import log from '../logger';
 import React from "react";
 import PropTypes from 'prop-types';
+import MsgElementsStore from '../msg-elements-store';
+import MsgInfoStore from '../msg-info-store';
 
 class TagFilter extends React.Component {
   constructor(props) {
     super(props);
+    this.msgElementStore = new MsgElementsStore();
+    this.msgInfoStore = new MsgInfoStore();
+  }
+
+  filterByTag(tagName) {
+    this.msgElementStore.recentElements().map((item) => {
+      const tags = this.msgInfoStore.recentTags()[item.msgId];
+      if (!(tags && tags.some(e => e.name === tagName))) {
+        $(item.msgEl).hide();
+
+        // If there are no more visible msgs in this msg's hc-chat-msg row, hide row
+        // item.msgEl is a div.actionable-msg-container
+        // Find the parent hc-chat-row above it
+        const parentHcChatRow = $(item.msgEl).closest('div.hc-chat-row');
+        // Inspect all child actionable-msg-containers to see if any have been .hide()ed
+        const childMsgs = $(parentHcChatRow).find('div.actionable-msg-container');
+        log("childMsgsLen=" + childMsgs.length);
+        if ($.makeArray($(childMsgs)).some((e) => $(e).is(":visible"))) {
+          log("Not OK to hide the parent");
+        }
+        else {
+          log("OK to hide the parent");
+          $(parentHcChatRow).hide();
+        }
+      }
+    });
+  }
+
+  clearFilterByTag() {
+    this.msgElementStore.recentElements().map((item) => {
+      $(item.msgEl).show();
+      // And show the parent container, in case it was also .hide()ed
+      $(item.msgEl).closest('div.hc-chat-row').show();
+    });
   }
 
   render() {
     if (this.props.tag) {
-      log('TODO: filter by tag: ' + this.props.tag.name)
+      this.filterByTag(this.props.tag.name);
     }
     else {
-      log('TODO: clear filter');
+      this.clearFilterByTag();
     }
 
     return null;
